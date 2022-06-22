1e10dcbeb952c2c9c262af4450b75aa8cd502396
Aaron francolino <aaron.e.francolino@gmail.com>
2022-06-22 10:25:56 -0300

    subiendo sistema de gestion en java con db

---
+++ src/main/java/com/mycompany/sistemagestion/SistemaGestion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sistemagestion;
+
+import com.mycompany.sistemagestion.form.Formulario;
+
+/**
+ *
+ * @author aaron
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario ();
+        ventana.show();
+    }
+}
